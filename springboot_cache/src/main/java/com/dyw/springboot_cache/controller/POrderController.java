2c6a99a22fcbe9365974b8bcb2c37a94e993bbfc
dengyw15 <ywdenguestc@gmail.com>
2019-12-25 16:11:20 +0800

    modify redis

--- springboot_cache/src/main/java/com/dyw/springboot_cache/controller/POrderController.java
+++ springboot_cache/src/main/java/com/dyw/springboot_cache/controller/POrderController.java
@@ -2,11 +2,14 @@ package com.dyw.springboot_cache.controller;
 
 import com.dyw.springboot_cache.bean.POrder;
 import com.dyw.springboot_cache.service.POrderService;
+import com.google.common.collect.Lists;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
 import java.util.List;
 
 @RestController
@@ -23,6 +26,15 @@ public class POrderController {
     @GetMapping("/order/list")
     public List<POrder> getAllOrders() {
         return pOrderService.findAllOrders();
+    }
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(1024);
+        sb.append("hello");
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+        sb.setLength(0);
+        System.out.println(sb);
     }
 
 }
